67d158da7d6ee7b4b4c4bff2f19a8be2675e56ac
Hoang Ta <blue.hoangta@gmail.com>
2024-08-09 09:25:40 +0700

    init project.

---
+++ WeatherForecast/WeatherForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForecastApp.swift
+//  WeatherForecast
+//
+//  Created by Hoang Ta on 9/8/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
