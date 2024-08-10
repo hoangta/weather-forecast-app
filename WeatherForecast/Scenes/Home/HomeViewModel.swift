89b4b38afe9b48c4b0ad50da8fcc95adb54beb97
Hoang Ta <blue.hoangta@gmail.com>
2024-08-10 14:40:56 +0700

    fix search results still lingers.

--- WeatherForecast/Scenes/Home/HomeViewModel.swift
+++ WeatherForecast/Scenes/Home/HomeViewModel.swift
@@ -22,14 +22,27 @@ extension HomeView {
             realm: Realm = DI.resolve()
         ) {
             cities = realm.objects(City.self)
-            
+
             $searchText
+            // Reduce network calls, also switch to background thread
                 .debounce(for: .seconds(0.5), scheduler: DispatchQueue.global())
-                .filter { $0.count >= 2 }
+            // There is an issue when binding searchText that cause every change emit 2 times
+            // This also happen when binding via TextField
+            // https://forums.swift.org/t/why-published-var-didset-is-called-extra-time-when-its-referenced-by-textfield-binding/52940/10
                 .removeDuplicates()
-                .map { apiClient.request(.geocoding($0), for: [City].self) }
+            // .map + .switchToLatest = .flatMapLatest in rxswift, i.e we only want the latest result
+                .map { text -> AnyPublisher<[City], Never> in
+                    // Only cater for search text with 2 or more character, otherwise return empty
+                    if text.count < 2 {
+                        return Just([]).eraseToAnyPublisher()
+                    } else {
+                        return apiClient.request(.geocoding(text), for: [City].self)
+                            .replaceError(with: [City]())
+                            .eraseToAnyPublisher()
+                    }
+                }
                 .switchToLatest()
-                .replaceError(with: [City]())
+            // Switch back to main thread for UI updates
                 .receive(on: DispatchQueue.main)
                 .assign(to: &$searchResults)
         }
